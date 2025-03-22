cd827f15c6c185611ec162619672c30c2f2f4ca1
MilosTanaskovic <srbinusvemiru@gmail.com>
2025-03-22 14:38:46 +0100

    feat: added account form

--- src/app/(dashboard)/settings/account/page.tsx
+++ src/app/(dashboard)/settings/account/page.tsx
@@ -1,5 +1,20 @@
-function Account() {
-  return <div className="">Account</div>;
+import { AccountForm } from "@/components/AccountForm";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/db/auth";
+
+async function Account() {
+  const session = await getServerSession(authConfig);
+  const user = await fetch(
+    `http://localhost:3000/api/users/${session?.user.id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const response = await user.json();
+  return <AccountForm user={response} />;
 }
 
 export default Account;
---
+++ src/components/AccountForm.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { AccountSchema } from "@/types/account-schema";
+import { toast } from "sonner";
+import {
+  Form,
+  FormMessage,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormField,
+} from "./ui/form";
+
+import { Input } from "./ui/input";
+
+import { Loader2 } from "lucide-react";
+import { User } from "@/types/user";
+import { updateUser } from "@/lib/updateUser";
+
+export const AccountForm = ({ user }: { user: User }) => {
+  //   const { data, error, isMutating } = useSWR(
+  //     "https://api.vercel.app/blog",
+  //     fetcher
+  //   );
+  const [isMutating, setisMutating] = useState(false);
+
+  const form = useForm<z.infer<typeof AccountSchema>>({
+    resolver: zodResolver(AccountSchema),
+    defaultValues: {
+      email: user.email,
+      password: "",
+      confirmPassword: "",
+      phone: user.phone,
+      lastName: user.lastName || "",
+      firstName: user.firstName,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof AccountSchema>) => {
+    setisMutating(true);
+    const response = await updateUser(user.id, {
+      email: values.email,
+      phone: values.phone,
+      lastName: values.lastName || "",
+      firstName: values.firstName,
+    });
+    if (response?.error) {
+      toast.error(response.error);
+      setisMutating(false);
+    } else {
+      toast.success("Account successfully updated!");
+      setisMutating(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit, (errors) => {
+          console.log("❌ Validation errors:", errors);
+        })}
+        className="space-y-6 text-center"
+      >
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First name</FormLabel>
+              <FormControl>
+                <Input placeholder="John" {...field} />
+              </FormControl>
+              <FormMessage className="text-left" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last name</FormLabel>
+              <FormControl>
+                <Input placeholder="Smith" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          disabled={true}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@mail.com" {...field} />
+              </FormControl>
+
+              <FormMessage className="text-left" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input placeholder="+12345678" {...field} />
+              </FormControl>
+
+              <FormMessage className="text-left" />
+            </FormItem>
+          )}
+        />
+        {/* <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="password" {...field} />
+              </FormControl>
+
+              <FormMessage className="text-left" />
+            </FormItem>
+          )}
+        /> */}
+        {/* <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm password</FormLabel>
+              <FormControl>
+                <Input placeholder="Re-enter password" {...field} />
+              </FormControl>
+
+              <FormMessage className="text-left" />
+            </FormItem>
+          )}
+        /> */}
+        <Button type="submit" className="w-full bg-green-700 cursor-pointer">
+          {isMutating && <Loader2 className="animate-spin" />}
+          Save
+        </Button>
+      </form>
+    </Form>
+  );
+};
