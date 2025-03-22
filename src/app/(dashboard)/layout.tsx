95fe53f844c2d8d8a890c306653fd50cf28c006b
MilosTanaskovic <srbinusvemiru@gmail.com>
2025-03-22 14:38:23 +0100

    chore: added  theme toggle

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,14 @@
 import { Separator } from "@/components/ui/separator";
 import AppSidebar from "@/components/Sidebar";
+import { ModeToggle } from "@/components/ModeToggle";
 
 async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex">
       <AppSidebar />
       <div className="flex flex-1 flex-col min-h-screen">
-        <header className="flex items-center justify-between container px-6 py-4 h-[64px]">
-          Orderly
+        <header className="flex items-center justify-end container px-6 py-4 h-[64px]">
+          <ModeToggle />
         </header>
         <Separator />
         <div className="overflow-auto h-full">
