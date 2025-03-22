c3754d44c1e5df8366fad7708540d75dbe8c9ec7
MilosTanaskovic <srbinusvemiru@gmail.com>
2025-03-22 15:01:02 +0100

    fix: modify sign in page color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
 async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="h-fit bg-slate-100 rounded-2xl p-8 min-w-[350px]">
-        {children}
-      </div>
+      <div className="h-fit rounded-2xl p-8 min-w-[350px]">{children}</div>
     </div>
   );
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,14 +7,12 @@ async function Layout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen flex">
       <AppSidebar />
       <div className="flex flex-1 flex-col min-h-screen">
-        <header className="flex items-center justify-end container px-6 py-4 h-[64px]">
+        <header className="flex items-center justify-end  px-6 py-4 h-[64px]">
           <ModeToggle />
         </header>
         <Separator />
         <div className="overflow-auto h-full">
-          <div className="flex-1 container p-4 text-accent-foreground">
-            {children}
-          </div>
+          <div className="flex-1  p-4 text-accent-foreground">{children}</div>
         </div>
       </div>
     </div>
